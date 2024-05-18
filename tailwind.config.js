5b46c80a793ebfc92a15889331eda0ec3858ef3b
Stilllee <orodae@gmail.com>
2024-05-19 02:12:32 +0900

    style: TailwindCSS 폰트 설정 추가 및 글로벌 CSS 파일 수정
    - Pretendard 폰트 파일 및 CSS 파일 추가
    - 글로벌 CSS 파일에서 폰트 임포트
    - TailwindCSS 설정 파일에 Typography 관련 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "ui-sans-serif", "system-ui"],
+      },
+      fontSize: {
+        title: ["2rem", "2.8rem"],
+        subtitle: ["1.6rem", "2.4rem"],
+        body: ["1.4rem", "2.2rem"],
+        button: ["1.6rem", "2rem"],
+        caption: ["1.2rem", "1.6rem"],
+        d1: ["4rem", "4.8rem"],
+        d2: ["3.2rem", "4.0rem"],
+        d3: ["2.8rem", "3.6rem"],
+        d4: ["2.4rem", "3.2rem"],
+        d5: ["2rem", "2.8rem"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        bold: 700,
+      },
+      colors: {
+        textCustom: "#171719",
+      },
+    },
   },
   plugins: [],
-}
+};
