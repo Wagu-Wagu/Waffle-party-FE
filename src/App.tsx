88367fdb4ac54fa35185fec559e12c4e3393898d
Stilllee <orodae@gmail.com>
2024-05-18 13:57:43 +0900

    [style] 불필요한 코드 제거 및 내보내기 코드 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import Router from "./Router";
 
-function App() {
-  const num = 1;
-  const num2 = 2;
-
-  const handleClickBtn = (n) => {
-    var sum = 0;
-    for (let i = 0; i < n; i++) {
-      sum += i;
-    }
-    console.log(sum);
-  };
-
-  const func = () => {
-    await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  };
-
-  const func2 = async () => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1");
-  };
-
-  console.log(num == num2);
+export default function App() {
   return (
     <>
       <Router />
     </>
   );
 }
-
-export default App;
