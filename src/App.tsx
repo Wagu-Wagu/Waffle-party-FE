aa0dc1ddde41e41389f7b323c71e931b8e8a362f
boeunLee <bounjaeun@gmail.com>
2024-05-18 13:54:34 +0900

    [test] eslint 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ function App() {
   const num = 1;
   const num2 = 2;
 
+  const handleClickBtn = (n) => {
+    var sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += i;
+    }
+    console.log(sum);
+  };
+
   const func = () => {
     await fetch("https://jsonplaceholder.typicode.com/todos/1");
   };
--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,13 @@ import { RecoilRoot } from "recoil";
 import Home from "./pages/Home";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <RecoilRoot>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                </Routes>
-            </RecoilRoot>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
 }
-
