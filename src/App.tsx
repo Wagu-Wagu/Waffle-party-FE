f3b541354988f672cf476c73dce4e610574d5e11
Stilllee <orodae@gmail.com>
2024-05-18 13:36:03 +0900

    [chore] eslint 적용 안되는 문제 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import Router from "./Router"
+import Router from "./Router";
 
 function App() {
+  const num = 1;
+  const num2 = 2;
 
+  const func = () => {
+    await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  };
+
+  const func2 = async () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1");
+  };
+
+  console.log(num == num2);
   return (
     <>
-      <Router/>
+      <Router />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
