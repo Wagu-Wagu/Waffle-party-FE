c99ee5bef705eded1656b83d5c6e3246057b238a
Stilllee <orodae@gmail.com>
2024-05-19 23:45:28 +0900

    [feat] 홈 페이지 헤더 연결
    #8

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,24 @@
+import Header from "../components/Header/Header";
+import HeaderLoginButton from "../components/Header/HeaderLoginButton";
+import LogoYellow from "./../assets/icons/LogoYellow.svg?react";
+
 export default function HomePage() {
-  return <>홈페이지</>;
+  return (
+    <>
+      <Header
+        leftChild={
+          <h1
+            aria-label="Waffle Party"
+            className="max-w-[8rem] max-h-[2.4rem] overflow-hidden flex items-center justify-center"
+          >
+            <div className="flex items-center justify-center w-full h-full">
+              <LogoYellow />
+            </div>
+          </h1>
+        }
+        rightChild={<HeaderLoginButton />}
+      />
+      홈페이지
+    </>
+  );
 }
