1542f4d2c9a38f5ae0591994c69b48b2ee2c2d2d
boeunLee <bounjaeun@gmail.com>
2024-05-20 03:23:03 +0900

    [style] 리스트 모달 컴포넌트 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import ListModal from "../components/common/ListModal";
+
 export default function HomePage() {
-  return <>홈페이지</>;
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
+  const clickModal = () => {
+    // window.alert("dd");
+    setIsBottomSheetOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <ListModal
+        isShow={isBottomSheetOpen}
+        type="comment"
+        onClick={clickModal}
+      />
+      <div onClick={clickModal}>Open Modal</div>
+    </>
+  );
 }
