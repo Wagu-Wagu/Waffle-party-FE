2c85b60bb27c7d178fc54988a1c490decaa2c1d5
boeunLee <bounjaeun@gmail.com>
2024-05-19 12:46:40 +0900

    [style] 버튼 컴포넌트 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
-import Button from "../components/Button";
+import Button from "../components/common/Button";
+import BasicLayout from "../layout/BasicLayout";
 
 export default function HomePage() {
   const handleClickBtn = () => {
@@ -6,45 +7,51 @@ export default function HomePage() {
   };
   return (
     <>
-      <div className="text-sm">홈페이지 입니다.</div>
-      <Button
-        className={"b-main"}
-        type="button"
-        disabled={false}
-        size="xs"
-        onClick={handleClickBtn}
-      >
-        버튼
-      </Button>
-      <Button
-        className={"f-main"}
-        type="button"
-        disabled={false}
-        size="base"
-        onClick={handleClickBtn}
-      >
-        버튼
-      </Button>
-      <div>비활성화 버튼</div>
-      <Button
-        className={"f-main"}
-        type="button"
-        disabled={true}
-        size="base"
-        onClick={handleClickBtn}
-      >
-        버튼
-      </Button>
-      <Button
-        className={"f-sub"}
-        type="button"
-        disabled={false}
-        size="sm"
-        onClick={handleClickBtn}
-      >
-        버튼
-      </Button>
-      <div className="text-base">dddd</div>
+      <BasicLayout>
+        <div className="text-sm">홈페이지 입니다.</div>
+        <Button
+          className=" text-danger bg-primary"
+          type="button"
+          disabled={false}
+          size="xs"
+          radius="base"
+          onClick={handleClickBtn}
+        >
+          버튼
+        </Button>
+        <Button
+          className="border border-success text-success text-subtitle"
+          type="button"
+          disabled={false}
+          size="base"
+          radius="base"
+          onClick={handleClickBtn}
+        >
+          버튼
+        </Button>
+        <br />
+        <div className="text-title">비활성화 버튼</div>
+        <Button
+          className="text-danger bg-primary"
+          type="button"
+          disabled={true}
+          size="base"
+          radius="sm"
+          onClick={handleClickBtn}
+        >
+          버튼
+        </Button>
+        <Button
+          className="text-base text-gray13 bg-success"
+          type="button"
+          disabled={false}
+          size="sm"
+          radius="xs"
+          onClick={handleClickBtn}
+        >
+          버튼
+        </Button>
+      </BasicLayout>
     </>
   );
 }
