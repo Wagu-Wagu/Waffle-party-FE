06f2667c81e6d7c9523b99491cf41206dc7dccc9
boeunLee <bounjaeun@gmail.com>
2024-05-20 05:19:18 +0900

    [style] 홈페이지 초기화

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,3 @@
-import { useState } from "react";
-import ListModal from "../components/common/ListModal";
-
 export default function HomePage() {
-  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-
-  const clickModal = () => {
-    // window.alert("dd");
-    setIsBottomSheetOpen((prev) => !prev);
-  };
-
-  return (
-    <>
-      <ListModal
-        isShow={isBottomSheetOpen}
-        type="comment"
-        onClick={clickModal}
-      />
-      <div onClick={clickModal}>Open Modal</div>
-    </>
-  );
+  return <>홈페이지</>;
 }
