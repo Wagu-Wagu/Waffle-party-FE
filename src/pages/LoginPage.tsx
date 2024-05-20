5a802dbeb937d96e562d1a2e70e5e3337bdd7d48
Stilllee <orodae@gmail.com>
2024-05-20 23:41:03 +0900

    [style] 새로 추가된 색상으로 텍스트 스타일 변경
    #15

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -28,9 +28,11 @@ export default function LoginPage() {
             <p className="text-gray7">로그인/회원가입</p>
             <div className="bg-gray7 w-[11.1rem] h-[0.1rem]" />
           </div>
-          <button className="mt-[2.5rem] mb-[7rem] flex items-center gap-4 bg-[#ffe001] py-[1.45rem] px-[9.5rem] rounded-2xl">
+          <button className="mt-[2.5rem] mb-[7rem] flex items-center gap-4 bg-additional1 py-[1.45rem] px-[9.5rem] rounded-2xl">
             <KakaoTalkIcon />
-            <span className="font-bold text-body">카카오로 시작하기</span>
+            <span className="font-bold text-body text-additional2">
+              카카오로 시작하기
+            </span>
           </button>
         </div>
       </main>
