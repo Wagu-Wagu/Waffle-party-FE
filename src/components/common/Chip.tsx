610acb51208388d3e0ddd2cce242af66552d6427
Stilllee <orodae@gmail.com>
2024-05-21 16:56:22 +0900

    [style] Chip 컴포넌트에 whitespace-nowrap 추가
    #13

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -14,7 +14,7 @@ export default function Chip({ ott, isCheck, onClick, isButton }: ChipProps) {
       className={`flex ${isButton ? "cursor-pointer" : ""}`}
     >
       <div
-        className={`inline-flex items-center justify-center gap-2 py-[0.8rem] px-[1.4rem] rounded-[5rem] ${isCheck ? "text-yellow5 border border-yellow5 bg-yellowTrans1" : "text-white bg-gray13"}`}
+        className={`whitespace-nowrap inline-flex items-center justify-center gap-2 py-[0.8rem] px-[1.4rem] rounded-[5rem] ${isCheck ? "text-yellow5 border border-yellow5 bg-yellowTrans1" : "text-white bg-gray13"}`}
       >
         {isCheck && <CheckChip />}
         {ott}
