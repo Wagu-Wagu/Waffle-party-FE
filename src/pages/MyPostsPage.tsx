673699ac13704ee44697d464f8cb610d780f2f71
Stilllee <orodae@gmail.com>
2024-05-21 16:46:21 +0900

    [feat] 빈 게시물 목록에 대한 컴포넌트 분리
    #13

--- src/pages/MyPostsPage.tsx
+++ src/pages/MyPostsPage.tsx
@@ -1,14 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header/Header";
 import HeaderButton from "../components/Header/HeaderButton";
 import PostListCard from "../components/Home/PostListCard";
 import LeftArrowIcon from "./../assets/icons/LeftArrowIcon.svg?react";
 import AlertCircleError from "./../assets/icons/AlertCirlcleError.svg?react";
-import { useNavigate } from "react-router-dom";
 import { PostList } from "./HomePage";
+import EmptyList from "../components/common/EmptyList";
 
 export default function MyPostsPage() {
   // 현재 로그인 한 사용자의 id를 비교해서 필터링
-  const filteredPostList = PostList.filter((post) => post.writer === "공상가");
+  const filteredPostList = PostList.filter((post) => post.writer === "");
   const nav = useNavigate();
 
   return (
@@ -21,17 +22,13 @@ export default function MyPostsPage() {
         }
         title="작성한 글"
       />
-      <main className="h-screen-minus-46]">
+      <main className="h-screen-minus-46">
         {filteredPostList.length === 0 && (
-          <div className="flex flex-col items-center justify-center h-full text-gray10">
-            <AlertCircleError />
-            <p className="font-medium text-[1.6rem] leading-[2.4rem] mt-4 mb-6">
-              작성하신 게시글이 없어요.
-            </p>
-            <p className="text-caption01 font-regular text-gray8">
-              첫 게시글을 작성해주세요.
-            </p>
-          </div>
+          <EmptyList
+            icon={<AlertCircleError />}
+            mainText="작성하신 게시글이 없어요"
+            subText={<p>첫 게시글을 작성해주세요</p>}
+          />
         )}
         {filteredPostList.map((post) => (
           <PostListCard key={post.id} post={post} />
