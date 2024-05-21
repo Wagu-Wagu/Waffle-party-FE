442fa2daf192e84854deee00cfee0e3fbc58d0b7
boeunLee <bounjaeun@gmail.com>
2024-05-21 14:04:32 +0900

    [style] 마이페이지  마크업 및 스타일링 완료

--- src/components/MyPage/MyPageSection.tsx
+++ src/components/MyPage/MyPageSection.tsx
@@ -8,7 +8,7 @@ export default function MyPageSection({ title, children }: myPageSectionProps) {
       <p className="h-[2.2rem] font-sans font-light leading-[2.2rem] text-gray8 text-[1.4rem] flex items-center">
         {title}
       </p>
-      <div className="flex flex-col gap-[2.7rem] leading-[2.4rem]">
+      <div className="flex flex-col gap-[2.8rem] leading-[2.4rem]">
         {children}
       </div>
     </div>
