73901be56ca2b425a59c8394004cf51927b0ed7e
Stilllee <orodae@gmail.com>
2024-05-22 14:55:20 +0900

    [feat] erd 기준으로 mock데이터 수정 및 분리
    #8

---
+++ src/__mocks__/mockData.ts
@@ -0,0 +1,187 @@
+export interface Post {
+  postId: number;
+  userId: number;
+  title: string;
+  content: string;
+  photoes: string[] | null;
+  ottTag: string;
+  thumbNail: string;
+  commentCount: number;
+  isActive: boolean;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+export interface User {
+  userId: number;
+  nickName: string;
+  email: string;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+export const PostList: Post[] = [
+  {
+    postId: 1,
+    userId: 1,
+    ottTag: "티빙",
+    title: "여고추리반3 보실 분",
+    content:
+      "무서운 저주가 떠도는 학교로 전학 간 추리반 학생들이 학교에 숨겨진 진실에 다가갈수록 더욱더 거대한 사건을 마주하면서 벌어지는 미스터리 어드벤처 여고추리반3 같이 봐요!",
+    photoes: [
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
+    ],
+    thumbNail:
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
+    commentCount: 26,
+    isActive: true,
+    createdAt: new Date("2024-05-19T00:00:00"),
+    modifiedAt: new Date("2024-05-19T00:00:00"),
+  },
+  {
+    postId: 2,
+    userId: 2,
+    ottTag: "넷플릭스",
+    title:
+      "와플맨의 3650가지 비밀 - 반죽의 황금비율과 소비자의 충동구매를 부추길 수 있는 요소들, 장인만의 떼돈 버는 특급 비법 그 어딘가를 찾아서 같이 보실 분",
+    content:
+      "와플장사로 제 2의 인생을 살아보려고 하는데, 같이 보실 자영업자 사장님들 구합니다.",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 30,
+    isActive: true,
+    createdAt: new Date("2024-05-19T00:00:00"),
+    modifiedAt: new Date("2024-05-19T00:00:00"),
+  },
+  {
+    postId: 3,
+    userId: 1,
+    ottTag: "티빙",
+    title: "여고추리반 시즌 1부터 시즌 3까지 함께 달려요! (스포 절대 금지)",
+    content: "스포 싫어요ㅜㅜ",
+    photoes: [
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
+    ],
+    thumbNail:
+      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
+    commentCount: 3,
+    isActive: true,
+    createdAt: new Date("2024-05-20T00:00:00"),
+    modifiedAt: new Date("2024-05-20T00:00:00"),
+  },
+  {
+    postId: 4,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 1",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 5,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 2",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 6,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 3",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 7,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 4",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 8,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된 5",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 9,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 6",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+  {
+    postId: 10,
+    userId: 3,
+    ottTag: "웨이브",
+    title: "윌터의 상상은 현실이 된다 7",
+    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
+    photoes: null,
+    thumbNail: "",
+    commentCount: 5,
+    isActive: true,
+    createdAt: new Date("2024-05-21T00:00:00"),
+    modifiedAt: new Date("2024-05-21T00:00:00"),
+  },
+];
+
+export const UserList: User[] = [
+  {
+    userId: 1,
+    nickName: "와플중독자",
+    email: "waffle@user.com",
+    createdAt: new Date("2024-01-01T00:00:00"),
+    modifiedAt: new Date("2024-01-01T00:00:00"),
+  },
+  {
+    userId: 2,
+    nickName: "와플입문자",
+    email: "newb@user.com",
+    createdAt: new Date("2024-01-01T00:00:00"),
+    modifiedAt: new Date("2024-01-01T00:00:00"),
+  },
+  {
+    userId: 3,
+    nickName: "공상가",
+    email: "visionary@user.com",
+    createdAt: new Date("2024-01-01T00:00:00"),
+    modifiedAt: new Date("2024-01-01T00:00:00"),
+  },
+];
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,117 +6,11 @@ import PostListCard from "../components/Home/PostListCard";
 import Navigation from "../components/Navigation/Navigation";
 import LogoYellow from "./../assets/icons/LogoYellow.svg?react";
 import Banner from "../components/Home/Banner";
-
-export const PostList = [
-  {
-    id: 1,
-    ott: "티빙",
-    title: "여고추리반3 보실 분",
-    content:
-      "무서운 저주가 떠도는 학교로 전학 간 추리반 학생들이 학교에 숨겨진 진실에 다가갈수록 더욱더 거대한 사건을 마주하면서 벌어지는 미스터리 어드벤처 여고추리반3 같이 봐요!",
-    thumbnail:
-      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
-    writer: "와플중독자",
-    date: "2024.5.18",
-    comments: 26,
-  },
-  {
-    id: 2,
-    ott: "넷플릭스",
-    title:
-      "와플맨의 3650가지 비밀 - 반죽의 황금비율과 소비자의 충동구매를 부추길 수 있는 요소들, 장인만의 떼돈 버는 특급 비법 그 어딘가를 찾아서 같이 보실 분",
-    content:
-      "와플장사로 제 2의 인생을 살아보려고 하는데, 같이 보실 자영업자 사장님들 구합니다.",
-    thumbnail: "",
-    writer: "와플입문자",
-    date: "2024.5.19",
-    comments: 30,
-  },
-  {
-    id: 3,
-    ott: "티빙",
-    title: "여고추리반 시즌 1부터 시즌 3까지 함께 달려요! (스포 절대 금지)",
-    content: "스포 싫어요ㅜㅜ",
-    thumbnail:
-      "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
-    writer: "추리중독자",
-    date: "2024.5.20",
-    comments: 3,
-  },
-  {
-    id: 4,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 5,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 6,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 7,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 8,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 9,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-  {
-    id: 10,
-    ott: "웨이브",
-    title: "윌터의 상상은 현실이 된다.",
-    content: "제 상상도 현실이 되었으면 좋겠네요. 같이 보실 분?",
-    thumbnail: "",
-    writer: "공상가",
-    date: "2024.5.21",
-    comments: 5,
-  },
-];
+import { PostList } from "../__mocks__/mockData";
 
 export default function HomePage() {
   const [selectedOtts, setSelectedOtts] = useState<string[]>([]);
+  const [postList, setPostList] = useState(PostList);
 
   const handleOttSelect = (ott: string) => {
     setSelectedOtts((prevSelectedOtts) =>
@@ -128,8 +22,8 @@ export default function HomePage() {
 
   const filteredPostList =
     selectedOtts.length > 0
-      ? PostList.filter((post) => selectedOtts.includes(post.ott))
-      : PostList;
+      ? postList?.filter((post) => selectedOtts.includes(post.ottTag))
+      : postList;
 
   return (
     <>
@@ -147,11 +41,11 @@ export default function HomePage() {
         rightChild={<HeaderLoginButton />}
         noBorder
       />
-      <main className="main">
+      <main className="main-header-nav">
         <Banner />
         <FilterList onOttSelect={handleOttSelect} selectedOtts={selectedOtts} />
-        {filteredPostList.map((post) => (
-          <PostListCard key={post.id} post={post} />
+        {filteredPostList?.map((post) => (
+          <PostListCard key={post.postId} post={post} />
         ))}
       </main>
       <Navigation />
