5da23f3233a73437a411400e35e71802b0753ba3
Stilllee <orodae@gmail.com>
2024-05-22 14:59:13 +0900

    [feat] 닉네임 가져오기, 날짜 변환 기능 추가
    #8

--- src/components/Home/PostListCard.tsx
+++ src/components/Home/PostListCard.tsx
@@ -1,28 +1,43 @@
 import { Link } from "react-router-dom";
 import MiniChip from "../common/MiniChip";
+import { Post, UserList } from "../../__mocks__/mockData";
 
-interface PostType {
-  id: number;
-  ott: string;
-  title: string;
-  content: string;
-  thumbnail: string;
-  writer: string;
-  date: string;
-  comments: number;
-}
+const getNickName = (userId: number): string => {
+  const user = UserList.find((user) => user.userId === userId);
+  return user ? user.nickName : "Unkown";
+};
 
-export default function PostListCard({ post }: { post: PostType }) {
-  const { id, ott, title, content, thumbnail, writer, date, comments } = post;
+const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString(undefined, options);
+};
+
+export default function PostListCard({ post }: { post: Post }) {
+  const {
+    postId,
+    userId,
+    ottTag,
+    title,
+    content,
+    thumbNail,
+    createdAt,
+    commentCount,
+  } = post;
+  const nickName = getNickName(userId);
+
   return (
     <Link
-      to={`/post-detail/${id}`}
+      to={`/post-detail/${postId}`}
       className="block p-8 border-b bg-gray14 border-gray13"
     >
-      <MiniChip ott={ott} />
+      <MiniChip ott={ottTag} />
       <div className="flex mt-[0.8rem]">
         <div
-          className={`flex-1 overflow-hidden ${thumbnail ? "pr-[1.2rem]" : ""}`}
+          className={`flex-1 overflow-hidden ${thumbNail ? "pr-[1.2rem]" : ""}`}
         >
           <p className="font-semibold text-white text-subtitle line-clamp-2">
             {title}
@@ -31,13 +46,13 @@ export default function PostListCard({ post }: { post: PostType }) {
             {content}
           </p>
           <div className="font-regular text-caption01 text-gray9">
-            <span>{writer}</span> · <span>{date}</span> ·{" "}
-            <span>댓글 {comments}개</span>
+            <span>{nickName}</span> · <span>{formatDate(createdAt)}</span> ·{" "}
+            <span>댓글 {commentCount}개</span>
           </div>
         </div>
-        {thumbnail && (
+        {thumbNail && (
           <img
-            src={thumbnail}
+            src={thumbNail}
             alt="thumbnail"
             className="w-[7.4rem] h-[7.4rem] rounded-[0.4rem] object-cover object-center"
           />
