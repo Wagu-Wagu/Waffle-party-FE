1900d1ede246794d0d62806f8e7edcb2b0401cac
boeunLee <bounjaeun@gmail.com>
2024-05-23 22:35:42 +0900

    [style] 내가 작성한 댓글 페이지 마크업 및 스타일링

--- src/pages/MyCommentsPage.tsx
+++ src/pages/MyCommentsPage.tsx
@@ -1,3 +1,44 @@
+import Header from "../components/Header/Header";
+import MyCommentsCard from "../components/card/MyCommentsCard";
+import LeftArrow from "../assets/icons/LeftArrowIcon.svg?react";
+import { myComment } from "../mock/myComment";
+import HeaderButton from "../components/Header/HeaderButton";
+import { useNavigate } from "react-router-dom";
+import EmptyList from "../components/common/EmptyList";
+import ChatIcon from "../assets/icons/Chat.svg?react";
 export default function MyCommentsPage() {
-  return <div></div>;
+  const nav = useNavigate();
+  const handleClickComment = (commentEl: any) => {
+    nav(`/post-detail/${commentEl.postId}`);
+  };
+  return (
+    <>
+      <Header
+        leftChild={
+          <HeaderButton onClick={() => nav("/", { replace: true })}>
+            <LeftArrow />
+          </HeaderButton>
+        }
+        title="작성한 댓글"
+        noBorder={false}
+      />
+      <div className="mt-[4.6rem]">
+        {myComment.length > 0 ? (
+          myComment.map((item, index) => (
+            <MyCommentsCard
+              data={item}
+              key={index}
+              onClick={(commentEl: any) => handleClickComment(commentEl)}
+            />
+          ))
+        ) : (
+          <EmptyList
+            icon={<ChatIcon />}
+            mainText="아직 댓글이 없어요."
+            subText="첫 댓글을 작성해보세요."
+          />
+        )}
+      </div>
+    </>
+  );
 }
