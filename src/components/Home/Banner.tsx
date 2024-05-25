179972f53f9ced3ed0b2bb3d87a01ff4ed049ed9
Stilllee <orodae@gmail.com>
2024-05-26 00:07:13 +0900

    [style] 배너 적용
    - 확정된 배너 적용
    - 배너 링크 연결

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -2,11 +2,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./../../styles/homeCustomSlick.css";
+import HeroImage01 from "./../../assets/icons/HeroImage01.svg";
+import HeroImage02 from "./../../assets/icons/HeroImage02.svg";
 
 const bannerImages = [
-  "https://image.tving.com/ntgs/operation/banner/2024/05/19/1716078320_1.jpg/dims/resize/F_webp,1920",
-  "https://search.pstatic.net/common/?src=https%3A%2F%2Fditto-phinf.pstatic.net%2F20240405_208%2F1712296500539nVxqY_JPEG%2F660f9233b8c2545456c85fd6.jpg&type=o&size=472x472&ttype=input",
-  "https://image.tving.com/ntgs/contents/CTC/caip/CAIP0900/ko/20231030/0505/P000643144.jpg/dims/resize/F_webp,400",
+  {
+    src: HeroImage01,
+    link: "https://www.notion.so/wisesky915/646dbbafa5ef4e8d9dd7dcbfd95e84eb",
+  },
+  {
+    src: HeroImage02,
+    link: "https://www.notion.so/wisesky915/ca41968629b048d6b44383543897620c",
+  },
 ];
 
 export default function Banner() {
@@ -25,13 +32,19 @@ export default function Banner() {
     <div className="relative custom-slick">
       <Slider {...settings} className="h-[15rem]">
         {bannerImages.map((image, index) => (
-          <div key={index} className="h-[15rem] overflow-hidden">
+          <a
+            href={image.link}
+            target="_blank"
+            key={index}
+            className="h-[15rem] overflow-hidden"
+          >
             <img
-              src={image}
+              src={image.src}
               alt={`banner-${index}`}
-              className="object-cover object-center w-full h-[15rem]"
+              // className="object-cover object-center w-full h-[15rem]"
+              className="w-full h-[15rem]"
             />
-          </div>
+          </a>
         ))}
       </Slider>
     </div>
