e5a95ee972735344de32fac7c4f3b84c247fe5de
boeunLee <bounjaeun@gmail.com>
2024-06-22 11:42:21 +0900

    [refactor] RecoilRoot 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { RecoilRoot } from "recoil";
 import Router from "./Router";
 import BasicLayout from "./layout/BasicLayout";
 
 export default function App() {
   return (
-    <>
+    <RecoilRoot>
       <BasicLayout>
         <Router />
       </BasicLayout>
-    </>
+    </RecoilRoot>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,5 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
@@ -23,34 +22,32 @@ const Loading = lazy(() => import("./components/Login/Loading"));
 export default function Router() {
   return (
     <BrowserRouter>
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/post-detail/:postId" element={<PostDetailPage />} />
-            <Route element={<PublicRoute />}>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/login/kakao/callback" element={<KakaoCallback />} />
-              <Route path="/nickname" element={<NicknamePage />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="/terms" element={<TermsPage />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-              <Route path="/post-create" element={<PostCreatePage />} />
-              <Route path="/post-edit/:postId" element={<PostEditPage />} />
-              <Route path="/notifications" element={<NotificationsPage />} />
-              <Route path="/mypage" element={<MyPage />} />
-              <Route path="/profile/edit" element={<ProfileEditPage />} />
-              <Route path="/mypage/post/:userId" element={<MyPostsPage />} />
-              <Route
-                path="/mypage/comment/:userId"
-                element={<MyCommentsPage />}
-              />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </Suspense>
-      </RecoilRoot>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post-detail/:postId" element={<PostDetailPage />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/login/kakao/callback" element={<KakaoCallback />} />
+            <Route path="/nickname" element={<NicknamePage />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/terms" element={<TermsPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/post-create" element={<PostCreatePage />} />
+            <Route path="/post-edit/:postId" element={<PostEditPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/profile/edit" element={<ProfileEditPage />} />
+            <Route path="/mypage/post/:userId" element={<MyPostsPage />} />
+            <Route
+              path="/mypage/comment/:userId"
+              element={<MyCommentsPage />}
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
