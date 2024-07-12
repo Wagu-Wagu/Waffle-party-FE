24e60e4a060c84dc70227c31e83fcb8cae7cb464
boeunLee <bounjaeun@gmail.com>
2024-07-12 12:30:19 +0900

    [feat] server.js 수정

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ async function startServer() {
     "/api",
     proxy("https://team-wagu-prod.com", {
       proxyReqPathResolver: (req) => {
-        return req.originalUrl.replace(/^\/api/, "");
+        // 요청 경로에서 '/api'를 제거하고 프록시할 주소를 만듭니다.
+        return req.originalUrl;
+      },
+      // 프록시 서버에서 CORS 문제를 해결하기 위해 필요한 옵션
+      proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+        proxyReqOpts.headers["Origin"] = srcReq.headers.origin;
+        return proxyReqOpts;
       },
     }),
   );
@@ -24,8 +30,11 @@ async function startServer() {
   // 모든 경로를 Vite에 전달
   app.use("*", vite.middlewares);
 
-  app.listen(5173, () => {
-    console.log("Server is running on http://localhost:5173");
+  let process;
+
+  const port = process.env.PORT || 5173;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
   });
 }
 
